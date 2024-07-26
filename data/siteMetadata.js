82c31e81fcc40e3cbfa6b21ebfa24f9cbe3623e7
Saugatkafley <saugatkafley@gmail.com>
2024-07-26 11:30:28 +0545

    change favicon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteLogo: '/static/images/algo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'saugatkafley@gmail.com',
